f92f48a502b9899784e224ccbcf285314f29819a
Lovinder Pnag <lspnag@gmail.com>
2016-10-02 08:28:21 -0700

    show view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,15 +2,22 @@ class MoviesController < ApplicationController
   def new
   end
 
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
   def create
     @movie = Movie.new params[:movie]    
 
-    @movie.save
-    redirect_to @movie
+    if @movie.save
+      redirect_to @movie
+    else
+      render 'new'
+    end
   end
 
   private
   def movie_params
-    params.require(:movie).permit(:)
+    params.require(:movie).permit(:title, :text)
   end
 end
